519eb7faf04e386c975bdf6b07aa29a338d6dfa7
Jed Chamberlain <jed@commerxia.io>
2025-09-02 17:20:03 +0100

    Adjust validation to check that all values are integers

--- run.py
+++ run.py
@@ -47,6 +47,7 @@ def validate_data(values):
     #print(values)
 
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
