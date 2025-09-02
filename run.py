b5990873a2b0dca099bbbc34b2e95337c7fbdedd
Jed Chamberlain <jed@commerxia.io>
2025-09-02 17:45:11 +0100

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -25,17 +25,26 @@ def get_sales_data():
     """
     get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60")
-    print()
+    while True: 
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60")
+        print()
 
-    data_str = input("Enter your data here: ")
-    #print(f"The data provided is {data_str}")
+        data_str = input("Enter your data here: ")
+        #print(f"The data provided is {data_str}")
 
-    sales_data = data_str.split(",")
-    #print(sales_data)
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        #print(sales_data)
+        #validate_data(sales_data)
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    
+    return sales_data
+
+
 
 def validate_data(values):
     
@@ -54,9 +63,11 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
 
+"""get_sales_data()"""
 
-
+data = get_sales_data()
 
